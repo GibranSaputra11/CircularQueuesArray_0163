80c1bc82d737d42b268f6d9a6f650e3a83d9ae5c
GibranSaputra11 <gibranbantul@gmail.com>
2025-06-12 10:57:27 +0700

    set default

--- ciecularQueues.cpp
+++ ciecularQueues.cpp
@@ -129,6 +129,10 @@ int main (){
                 case '4':{
                     return 0;
                 }
+                default:{
+                    cout << "Invalid option!!!!!" << endl;
+                    break;
+                }
             }
         }
     }
